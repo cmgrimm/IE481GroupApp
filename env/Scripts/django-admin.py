dfec94398f2363d96270e68dbfc90a128cccd4a1
cmgrimm <cmgrimm@iastate.edu>
2016-11-16 15:10:53 -0600

    virtual environment

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Chase\Desktop\DesktopFolders\481Project\IE481GroupApp\IE481GroupApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
